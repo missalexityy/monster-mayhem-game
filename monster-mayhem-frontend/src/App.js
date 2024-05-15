48f478b3791ade50b437077da74e9b2256bdc7cb
Alessandra <2021258@student.cct.ie>
2024-05-15 09:10:30 +0100

    ERROR in ./src/App.js 17:37-43
    Tried to
    -use 'Routes' instead of 'Switch'
    - 'element' prop instead of 'component'

--- monster-mayhem-frontend/src/App.js
+++ monster-mayhem-frontend/src/App.js
@@ -1,6 +1,6 @@
 // Import necessary dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // React Router for routing
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // React Router for routing
 import RegistrationForm from './RegistrationForm'; // Import RegistrationForm component
 import GamePlay from './GamePlay'; // Import GamePlay component
 import Statistics from './Statistics'; // Import Statistics component
@@ -12,14 +12,14 @@ const App = () => {
     <Router>
       <div>
         {/* Define routes */}
-        <Switch>
+        <Routes>
           {/* Route for player registration */}
-          <Route path="/register" component={RegistrationForm} />
+          <Route path="/register" element={<RegistrationForm />} />
           {/* Route for playing the game */}
-          <Route path="/play" component={GamePlay} />
+          <Route path="/play" element={<GamePlay />} />
           {/* Route for displaying player statistics */}
-          <Route path="/stats" component={Statistics} />
-        </Switch>
+          <Route path="/stats" element={<Statistics />} />
+        </Routes>
       </div>
     </Router>
   );
